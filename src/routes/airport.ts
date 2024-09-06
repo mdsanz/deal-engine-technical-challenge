d111cc2dcc968e9d02f32974797e505e8076b398
Marcos Sanchez <marcos.sanzlara@gmail.com>
2024-09-06 00:50:40 -0600

    Agregando getCountByOriginDestination endpoint y mejorando un poco el controller

--- src/routes/airport.ts
+++ src/routes/airport.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import {
+    getCountByOriginDestination,
     getFlights,
     getTotalUniqueFlights,
 } from "../controllers/flight.controller";
@@ -8,5 +9,6 @@ const router = Router();
 
 router.get("/", getFlights);
 router.get("/unique-count", getTotalUniqueFlights);
+router.get("/count-by-origin-destination", getCountByOriginDestination);
 
 export { router };
